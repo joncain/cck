0ec8d43bc0df6f6a0b6447221b991f0826640b5e
Jon Cain <jon@joncain.net>
2017-11-15 19:55:22 -0700

    Import files

---
+++ functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+print_error()
+{
+  echo "Error: $1"
+  exit 1
+}
+
+ensure_date_arg()
+{
+  #
+  # Ensure we have the import date arg
+  #
+  if [ "$1" = "" ]; then
+   print_error "Missing argument - Pass the import date"
+  fi
+}
+
+parse_import_date()
+{
+  #
+  # Parse the import date
+  #
+  IMPORT_DATE=`(date "+%Y-%m-%d" --date="$1") 2> /dev/null`
+}
