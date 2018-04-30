5539b1e66175eb29b2102d7533caf350e0aadbb0
Jon Cain <jon@joncain.net>
2018-04-30 09:07:47 -0600

    Changed date tag

--- tag_all_audio.sh
+++ tag_all_audio.sh
@@ -1,20 +1,17 @@
 #! /bin/bash
 
 ARTIST="Christopher Bent"
-VENUE="Calvary Chapel Kuna"
 
 for d in `ls -1d 2018*`
 do
-  FILE_DATE=`date --date="${d}" "+%d%m"`
-  FILE_YEAR=`date --date="${d}" "+%Y"`
-
   for f in `ls -1 ${d}/processed/${d}_*.mp3`
   do
     TITLE=`mid3v2 -l $f | grep TIT2 | cut -d "=" -f 2`
     ALBUM=`mid3v2 -l $f | grep TALB | cut -d "=" -f 2`
 
-    #mid3v2 -v --TPE1 "$ARTIST" --TCON "Speech" --TDRC "${FILE_YEAR}" --TDAT "${FILE_DATE}" --TPE3 "The Holy Spirit" --TALB "$TITLE" $f
-    mid3v2 -l $f
+    #mid3v2 -v --TPE1 "$ARTIST" --TCON "Speech" --TDRC "$d" --TPE3 "The Holy Spirit" --TALB "$TITLE" $f
+    #mid3v2 -v --TDRC "$d" $f
+    #mid3v2 -l $f
     #mid3v2 --delete-frames="TXXX" $f
   done  
 done
