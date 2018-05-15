775a6629e28228018bc565408eaffa8182a38203
Jon Cain <jon@joncain.net>
2018-05-15 13:55:03 -0600

    -Rename script -Clean up code

--- archive/import-archive.sh
+++ archive/import-archive.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-#./import_archive.sh '/media/jon/2017-05-13 1045/Music' '*-2*Romans*.mp3'
+#./import-archive.sh '/media/jon/2017-05-13 1045/Music' '*-2*Romans*.mp3'
 
 home=`pwd`
 
-path="${1}"
-pattern="${2}"
+path="$1"
+pattern="$2"
 
-cd "${path}"
-echo "home: ${home}"
+cd "$path"
+echo "home: $home"
 echo "where to look: `pwd`"
-echo "what to find: ${pattern}"
+echo "what to find: $pattern"
 echo "-------------------------------------------------------"
 
-for f in ${pattern}
+for f in "$pattern"
 do
   recording_date=`date "+%Y-%m-%d" --date="${f:4:2}-${f:0:2}-${f:2:2}"`
-  album=`echo "${f}" | cut -d " " -f 2`
+  album=`echo "$f" | cut -d " " -f 2`
   title=`echo "${f/.mp3/}" | awk '{ print $3 ":" $4 }'`
   
   if [ ! -e "$home/$recording_date" ]; then
@@ -31,17 +31,17 @@ do
 
   if [ ! -e "$home/$recording_date/processed/$f" ]; then
     file_path="$home/$recording_date/processed/${recording_date}_2.mp3"
-    echo "cp $f ${file_path}"
-    cp "$f" "${file_path}"
-    chmod 744 "${file_path}"
-    mid3v2 --delete-all "${file_path}" 
-    mid3v2 --TALB "${album}" --TIT2 "${title}" --TDRC "$recording_date" --TPE1 "Pastor Chris Bent" --TPE3 "The Holy Spirit" --TCON "Speech" "${file_path}"
+    echo "cp $f $file_path"
+    cp "$f" "$file_path"
+    chmod 744 "$file_path"
+    mid3v2 --delete-all "$file_path" 
+    mid3v2 --TALB "$album" --TIT2 "$title" --TDRC "$recording_date" --TPE1 "Pastor Chris Bent" --TPE3 "The Holy Spirit" --TCON "Speech" "$file_path"
   fi
 
   #echo $f
-  #mid3v2 -l "${file_path}" 
-  echo "album: ${album}"
-  echo "title: ${title}"
+  mid3v2 -l "$file_path" 
+  echo "album: $album"
+  echo "title: $title"
   echo
 done
 
