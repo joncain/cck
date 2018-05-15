8d22c61683a7ebd2348d94b348edbbdee6549ece
Jon Cain <jon@joncain.net>
2018-05-15 14:13:50 -0600

    Fixed pattern expression

--- archive/import-archive.sh
+++ archive/import-archive.sh
@@ -13,24 +13,24 @@ echo "where to look: `pwd`"
 echo "what to find: $pattern"
 echo "-------------------------------------------------------"
 
-for f in "$pattern"
+for f in ${pattern}
 do
   recording_date=`date "+%Y-%m-%d" --date="${f:4:2}-${f:0:2}-${f:2:2}"`
   album=`echo "$f" | cut -d " " -f 2`
   title=`echo "${f/.mp3/}" | awk '{ print $3 ":" $4 }'`
-  
-  if [ ! -e "$home/$recording_date" ]; then
+  file_path="$home/$recording_date/processed/${recording_date}_2.mp3"
+
+  if [ ! -d "$home/$recording_date" ]; then
     echo "mkdir \"$home/$recording_date\""
     mkdir "$home/$recording_date"
   fi
 
-  if [ ! -e "$home/$recording_date/processed" ]; then
+  if [ ! -d "$home/$recording_date/processed" ]; then
     echo "mkdir \"$home/$recording_date/processed\""
     mkdir "$home/$recording_date/processed"
   fi
 
-  if [ ! -e "$home/$recording_date/processed/$f" ]; then
-    file_path="$home/$recording_date/processed/${recording_date}_2.mp3"
+  if [ ! -e "$home/$recording_date/processed/${recording_date}_2.mp3" ]; then
     echo "cp $f $file_path"
     cp "$f" "$file_path"
     chmod 744 "$file_path"
@@ -38,7 +38,6 @@ do
     mid3v2 --TALB "$album" --TIT2 "$title" --TDRC "$recording_date" --TPE1 "Pastor Chris Bent" --TPE3 "The Holy Spirit" --TCON "Speech" "$file_path"
   fi
 
-  #echo $f
   mid3v2 -l "$file_path" 
   echo "album: $album"
   echo "title: $title"
